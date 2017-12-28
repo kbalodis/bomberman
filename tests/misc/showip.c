82bf2b2e89273a0eb381bf95df061014fea13e9a
Juris Liepins <juris.liepins@knowledgeprice.com>
2017-12-28 17:33:46 +0200

    showip comments

--- tests/misc/showip.c
+++ tests/misc/showip.c
@@ -7,6 +7,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+// showip performs DNS lookup of the hostname and prints the IP adresses of the remote server
+
 void 
 print_usage(void) 
 {
@@ -20,31 +22,35 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    char *hostname = argv[1];
+    char *hostname = argv[1];           // E.g. www.google.com.
 
+    // *servinfo will hold the result of getaddrinfo call.
+    // *p iterator for reading the linked list addrinfo.
+    // *servinfo is the head node in the linked list, getarrdinfo() call initializes the linked list of addrinfo structs.
     struct addrinfo hints;
     struct addrinfo *servinfo, *p;
+                                        
+    memset(&hints, 0, sizeof(hints));   // Zero out memory to make sure that getaddrinfo() initializes servinfo correctly.
 
-    memset(&hints, 0, sizeof(hints));
-
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_UNSPEC;        // IPv4 or IPv6, doesn't matter.
+    hints.ai_socktype = SOCK_STREAM;    // Use the TCP protocol (UDP is SOCK_DGRAM).
 
     int status;
 
-    if ((status = getaddrinfo(hostname, NULL, &hints, &servinfo)) != 0) {
+    if ((status = getaddrinfo(hostname, NULL, &hints, &servinfo)) != 0) {           // Second arg is (char*) port number.
         fprintf(stderr, "!! getaddrinfo error: %s !!\n", gai_strerror(status));
         exit(2);
     }
 
     fprintf(stdout, "--- IP addresses for '%s' ---\n", hostname);
 
-    char ipstr[INET6_ADDRSTRLEN];
+    char ipstr[INET6_ADDRSTRLEN];                       // Long enough to hold IPv6 results.
 
-    for (p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {     // Iterate over the resulting linked list.
         void *addr;
         char *ipver;
 
+        // Cast required to set the pointer in the correct location.
         if (p->ai_family == AF_INET) {
             struct sockaddr_in *ipv4 = (struct sockaddr_in*)p->ai_addr;
             addr = &(ipv4->sin_addr);
@@ -56,9 +62,9 @@ int main(int argc, char *argv[])
             ipver = "IPv6";
         }
 
-        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));    // Convert addr to a string.
 
-        fprintf(stdout, "  %s: %s\n", ipver, ipstr);
+        fprintf(stdout, "  %s: %s\n", ipver, ipstr);            // Print the ip.
     }
 
     freeaddrinfo(servinfo);
